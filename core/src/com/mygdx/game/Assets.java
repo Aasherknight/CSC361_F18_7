57f210d0660a51c8f6f6d24c1120e7b3651576c5
Andrew Stake <andrewms98@live.com>
2018-09-15 13:13:55 -0400

    p149 and p150. Adds asset class

---
+++ core/src/com/mygdx/game/Assets.java
@@ -0,0 +1,63 @@
+/**
+ * Andrew Stake
+ */
+
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.assets.AssetErrorListener;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Disposable;
+import com.mygdx.game.util.Constants;
+
+public class Assets implements Disposable, AssetErrorListener
+{
+	public static final String TAG = Assets.class.getName();
+	
+	public static final Assets instance = new Assets();
+	
+	private AssetManager assetManager;
+	
+	//singleton: prevent instantiation from other classes
+	private Assets() {}
+	
+	public void init(AssetManager assetManager)
+	{
+		this.assetManager = assetManager;
+		//set assent manager error handler
+		assetManager.setErrorListener(this);
+		//load texture atlas
+		assetManager.load(Constants.TEXTURE_ATLAS_OBJECTS, TextureAtlas.class);
+		//start loading assets and wait until finished
+		assetManager.finishLoading();
+		Gdx.app.debug(TAG, "# of assets loaded: " + assetManager.getAssetNames());
+		for (String a : assetManager.getAssetNames())
+			Gdx.app.debug(TAG, "asset: " + a);
+	}
+	
+	@Override
+	public void dispose()
+	{
+		assetManager.dispose();
+	}
+	
+	//Removed @Override due to no error method with these parameter is AssetErrorListener interface
+	public void error(String filename, Class type, Throwable throwable)
+	{
+		Gdx.app.error(TAG, "Couldn't load asset '" + filename + "'", (Exception)throwable);
+	}
+	
+	@Override
+	public void error(AssetDescriptor asset, Throwable throwable)
+	{
+		Gdx.app.error(TAG, "Couldn't load asset '" + asset.fileName + "'", (Exception)throwable);
+	}
+}
+
+
+
+
+
+
