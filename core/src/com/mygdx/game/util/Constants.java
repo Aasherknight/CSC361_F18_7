3b9dea28221ad67cf2fd33c7827f04fa5253326a
Andrew Stake <andrewms98@live.com>
2018-09-16 20:34:29 -0400

    Fixed texture atlas and asset loading

--- core/src/com/mygdx/game/util/Constants.java
+++ core/src/com/mygdx/game/util/Constants.java
@@ -10,7 +10,7 @@ public class Constants
 	public static final float VIEWPORT_HEIGHT = 5.0f;
 
 	//Location of description file for the texture atlas
-	public static final String TEXTURE_ATLAS_OBJECTS =  "..\\core\\assets\\canyonbunny.pack.atlas";
+	public static final String TEXTURE_ATLAS_OBJECTS =  "../core/assets/canyonbunny.atlas";
 	
 	public static final String LEVEL_01 = "levels/level-01.png";
 }
